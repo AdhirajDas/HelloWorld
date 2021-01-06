6f918bf43bd404a758b5f192ed03783bc1c6dff4
AdhirajDas <77044473+AdhirajDas@users.noreply.github.com>
2021-01-06 12:50:13 +0530

    Add files via upload

---
+++ Hello_World/Hello_World.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Hello_World
+{
+ String sixty_nine;
+  String PP_SizeCalculator(String name)
+  {
+       if(name == "Samuel Miller")
+       sixty_nine="Fuck off bitch";
+         else
+         if(name == "Johnny Sins")
+          sixty_nine="Hello World";
+           else
+            sixty_nine="You have a small pp";
+      return sixty_nine;
+  }
+   public static void main (String args[])
+   {
+       Scanner sc= new Scanner(System.in);
+       System.out.println("Enter your name noob coder");
+       String x;
+       x=sc.nextLine();
+       Hello_World ob = new Hello_World();
+       x=ob.PP_SizeCalculator(x);
+       System.out.println(x);
+    }
+}
